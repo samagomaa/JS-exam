ea8ccb6ac5112ab51d1d7c8a27ba2f3df7fdf1a7
samagomaa <samagoma662@gmail.com>
2024-09-02 05:23:36 +0300

    adding modal

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+    getmovie()
+    let boxwidth = $(".sideNavMenu .navTab").outerWidth()
+    $(".sideNavMenu").animate({left:`-${boxwidth}px`} , 200)
+    $(".loadingScreen").fadeOut(500)
+})
+
+
 let allMovies = []
 
 async function getmovie(statue = "trending/all/day"){
@@ -7,80 +15,131 @@ async function getmovie(statue = "trending/all/day"){
     displayMovies();
 }
 
-getmovie()
-
 function displayMovies() {
     var cartona = ``
     for(let i = 0 ; i < (allMovies).length  ; i++){
-        if(allMovies[i].title){
-            cartona += `<div class="col-md-4 p-3">
-            <div class="movie position-relative overflow-hidden ">
-                <img class="w-100" src="https://image.tmdb.org/t/p/w500/${allMovies[i].poster_path}" >
-                <div class="movie-layer position-absolute py-4 px-4">
-                    <h2 class="px-2">${allMovies[i].title}</h2>
-                    <p>${allMovies[i].overview}</p>
-                        <p>Release Date <span> : ${allMovies[i].first_air_date}</span></p>
-                        <div class="rate">
-                            <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
-                            <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
-                            <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
-                            <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
-                            <i class="fa-solid fa-star-half-stroke py-3 " style="color: #f0dc00;"></i>
-                            <h4 class="rounded-5 text-center py-2">${(allMovies[i].vote_average).toFixed(1)}</h4>
-                        </div>
+        if(allMovies[i].poster_path != null){
+            cartona += `<div class="col-md-4">
+            <div movieId="${allMovies[i].id}" data-bs-toggle="modal" data-bs-target="#exampleModal" class="movie position-relative overflow-hidden">
+                <img class="w-100" src="https://image.tmdb.org/t/p/w500/${allMovies[i].poster_path}"  >
+                <div class="movie-layer position-absolute">
+                <div class="p-4">
+                <div class="text-center">
+                <h2 class="px-2">${allMovies[i].name||allMovies[i].title}</h2>
                 </div>
-            </div>
-        </div>`
-        }else{
-            cartona += `<div class="col-md-4 p-3">
-            <div class="movie position-relative overflow-hidden ">
-                <img class="w-100" src="https://image.tmdb.org/t/p/w500/${allMovies[i].poster_path}" >
-                <div class="movie-layer position-absolute py-4 px-4">
-                    <h2 class="px-2">${allMovies[i].name}</h2>
-                    <p>${allMovies[i].overview}</p>
-                        <p>Release Date <span> : ${allMovies[i].first_air_date}</span></p>
-                        <div class="rate">
+                <div>
+                <p>${allMovies[i].overview.split(/\s+/).slice(0, 30).join(" ") + " ..."}</p>
+                </div>
+                <div>
+                <p>Release Date <span> : ${allMovies[i].first_air_date||allMovies[i].release_date}</span></p>
+                </div>
+                <div>
                             <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
                             <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
                             <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
                             <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
                             <i class="fa-solid fa-star-half-stroke py-3 " style="color: #f0dc00;"></i>
-                            <h4 class="rounded-5 text-center py-2">${(allMovies[i].vote_average).toFixed(1)}</h4>
-                        </div>
+                </div>
+                <div class="rate">
+                <h4 class="rounded-circle text-center py-2">${(allMovies[i].vote_average).toFixed(1)}</h4>
+                </div>
+                </div>
                 </div>
             </div>
         </div>`
         }
     }
     document.getElementById('rowDate').innerHTML = cartona
+    getmovieId()
 }
+
+
+function getmovieId(){
+    let movieList = document.querySelectorAll(".movie")
+        movieList.forEach((el)=>{
+            el.addEventListener("click",function(){
+                let id = this.getAttribute("movieId")
+                getmovieDetails(id)
+                console.log("herreee");
+                
+            })
+        })
+}
+
+function getmovieDetails(id){
+    allMovies.forEach((el)=>{
+        if(el.id == id){
+            displayMovieModal(el)
+            }
+        
+    })
+}
+
+function displayMovieModal(x){
+    console.log(x);
+    let cartona = ``
+        cartona += `<div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">${x.title||x.name}</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="container">
+              <div class="row g-4">
+                <div class="col-md-6">
+                    <div>
+                       <img class="w-100" src="https://image.tmdb.org/t/p/w500/${x.poster_path}"  >
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div>
+                        <p>Release Date :<span> ${x.first_air_date||x.release_date}</span></p>
+                        </div>
+                        <div>
+                                    <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
+                                    <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
+                                    <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
+                                    <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
+                                    <i class="fa-solid fa-star-half-stroke py-3 " style="color: #f0dc00;"></i>
+                        </div>
+                        <div class="rate">
+                        <h4 class="rounded-circle text-center py-2">${(x.vote_average).toFixed(1)}</h4>
+                        </div>
+                </div>
+                <div class="col-md-12">
+                    <div>
+                        <p>${x.overview}</p>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>`
+                document.getElementById('movieModal').innerHTML = cartona
+}
+
 
 function closeSideNav() {
     let boxwidth = $(".sideNavMenu .navTab").outerWidth()
-        $(".sideNavMenu").animate({left:-boxwidth} , 500)
-        $(".openCloseIcon").addClass("fa-align-justify")
-        $(".openCloseIcon").removeClass("fa-x")
+        $(".sideNavMenu").animate({left:`-${boxwidth}px`} , 500)
+        $(".CloseIcon").addClass("fa-align-justify")
+        $(".CloseIcon").removeClass("fa-x")
         $(".links li").animate({top:300} , 500)
 }
 
-closeSideNav()
-
 function openSideNav(){
-    $(".sideNavMenu").animate({left:0} , 500)
-    $(".openCloseIcon").removeClass("fa-align-justify");
-    $(".openCloseIcon").addClass("fa-x")
+    $(".sideNavMenu").animate({left:"0px"} , 500)
+    $(".CloseIcon").removeClass("fa-align-justify");
+    $(".CloseIcon").addClass("fa-x")
     for(i=0 ; i<6 ; i++){
         $(".links li").eq(i).animate({top:0} , (i+5)*100)
     }
 }
 
-$(".sideNavMenu i.openCloseIcon").click(()=>{
+$(".sideNavMenu i.CloseIcon").click(()=>{
     if($(".sideNavMenu").css("left") == "0px"){
-    closeSideNav()
+        closeSideNav()
     }else{
         openSideNav()
     }
-    
 })
 
 let searchResults = []
@@ -89,31 +148,34 @@ async function searchMovie(term) {
     let response = await fetch(`https://api.themoviedb.org/3/search/collection?api_key=eba8b9a7199efdcb0ca1f96879b83c44&query=${term}`)
     response = await response.json()
     searchResults = response.results
-    console.log(searchResults);
     searchResults ?  showResults(searchResults) : showResults([]);
 }
 
 function showResults(term) {
     var cartona = ``
-    for(let i = 0 ; i < term.length  ; i++){
-        cartona += `<div class="col-md-4 p-3">
-        <div class="movie position-relative overflow-hidden ">
-            <img class="w-100" src="https://image.tmdb.org/t/p/w500/${term[i].poster_path}" >
-            <div class="movie-layer position-absolute py-4 px-4">
-                <h2 class="px-2">${term[i].name}</h2>
+    if (term.length == 0) {
+        getmovie()
+    }else{
+        for(let i = 0 ; i < term.length  ; i++){
+            if(term[i].poster_path != null){
+                cartona += `<div class="col-md-4">
+            <div movieId="${term[i].id}" class="movie position-relative overflow-hidden">
+                <img class="w-100" src="https://image.tmdb.org/t/p/w500/${term[i].poster_path}"  >
+                <div class="movie-layer position-absolute">
+                <div class="p-4">
+                <div class="text-center">
+                <h2 class="px-2">${term[i].name||term[i].title}</h2>
+                </div>
+                <div>
                 <p>${term[i].overview}</p>
-                    <p>Release Date <span> : ${term[i].first_air_date}</span></p>
-                    <div class="rate">
-                        <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
-                        <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
-                        <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
-                        <i class="fa-solid fa-star py-3" style="color: #f0dc00;"></i>
-                        <i class="fa-solid fa-star-half-stroke py-3 " style="color: #f0dc00;"></i>
-                        <h4 class="rounded-5 text-center py-2">${(term[i].vote_average)}</h4>
-                    </div>
+                </div>
+                </div>
+                </div>
             </div>
-        </div>
-    </div>`
+        </div>`
+            }
+            
+        }
     }
     document.getElementById('rowDate').innerHTML = cartona
 }
